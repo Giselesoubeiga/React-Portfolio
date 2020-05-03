211bf25bbdec99a235c39030378bb2cd73c1229a
Gisele <giskey11@gmail.com>
2020-05-03 14:22:32 -0500

    Add components

---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+  class Container extends React.Component {
+      render(){
+          return(
+              <div>
+                  <h1>Container</h1>
+              </div>
+          )
+      }
+  }
+
+
+  export default Container
