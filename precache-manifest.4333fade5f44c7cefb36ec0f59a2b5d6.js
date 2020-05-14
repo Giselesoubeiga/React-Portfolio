2994337c6cc380ddd05da44360a4313ba9b31946
Gisele <giskey11@gmail.com>
2020-05-14 13:57:00 -0500

    Updates

---
+++ precache-manifest.4333fade5f44c7cefb36ec0f59a2b5d6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7a259365a954bc26580808fdb43a5c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "11412bf055cdef776080",
+    "url": "/React-Portfolio/static/css/main.64f5b025.chunk.css"
+  },
+  {
+    "revision": "2af5fb308ea9397e248a",
+    "url": "/React-Portfolio/static/js/2.b8a8746f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.b8a8746f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11412bf055cdef776080",
+    "url": "/React-Portfolio/static/js/main.4e31991c.chunk.js"
+  },
+  {
+    "revision": "b476918de5ca6530e65f",
+    "url": "/React-Portfolio/static/js/runtime-main.bd277262.js"
+  },
+  {
+    "revision": "d4f0a41c6d7a15455dcc15212f109851",
+    "url": "/React-Portfolio/static/media/GiseleSoubeigaResume.d4f0a41c.pdf"
+  },
+  {
+    "revision": "cf7485f028dd71ba8ac256a06798f656",
+    "url": "/React-Portfolio/static/media/Passwordgenerator.cf7485f0.PNG"
+  },
+  {
+    "revision": "3c024b9584472ed9a42fa1372464544c",
+    "url": "/React-Portfolio/static/media/Vibe.3c024b95.PNG"
+  },
+  {
+    "revision": "756ba2a213b311ed0e8b70191b128d58",
+    "url": "/React-Portfolio/static/media/Workdayscheduler.756ba2a2.PNG"
+  },
+  {
+    "revision": "7407e4c358491c332a3d934614fa3348",
+    "url": "/React-Portfolio/static/media/archer.7407e4c3.PNG"
+  },
+  {
+    "revision": "a449a0bd552002d74b310964b16b8618",
+    "url": "/React-Portfolio/static/media/bg.a449a0bd.jpg"
+  },
+  {
+    "revision": "2ab1075a8b5da8cee8c8561427ffecad",
+    "url": "/React-Portfolio/static/media/burgerApp.2ab1075a.PNG"
+  },
+  {
+    "revision": "456fe3c5094946e98af8f9e021e08bac",
+    "url": "/React-Portfolio/static/media/employeeDirectory.456fe3c5.PNG"
+  },
+  {
+    "revision": "7a9e1da60af8ec609af07379e663ab22",
+    "url": "/React-Portfolio/static/media/git.7a9e1da6.PNG"
+  },
+  {
+    "revision": "a1d85b2f166e3da8bed6ea27d1b3b136",
+    "url": "/React-Portfolio/static/media/link.a1d85b2f.PNG"
+  },
+  {
+    "revision": "982c11bcb7c048214b7e2f9c0e14eb52",
+    "url": "/React-Portfolio/static/media/profil.982c11bc.jpg"
+  },
+  {
+    "revision": "8587ce5bfa47c4c4671109ddf3bc8e8d",
+    "url": "/React-Portfolio/static/media/water.8587ce5b.jpg"
+  }
+]);
